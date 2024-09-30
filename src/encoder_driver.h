94c04a9dbf044bcc9fa53e61e9c8de7e4b9238b7
Vlad <vsukhachev@gmail.com>
2024-09-30 10:00:41 -0400

    feat: Update encoder pin assignments

--- src/encoder_driver.h
+++ src/encoder_driver.h
@@ -10,8 +10,8 @@
   #define LEFT_ENC_PIN_B PD3  //pin 3
   
   //below can be changed, but should be PORTC pins
-  #define RIGHT_ENC_PIN_A PC4  //pin A4
-  #define RIGHT_ENC_PIN_B PC5   //pin A5
+  #define RIGHT_ENC_PIN_A PC5  //pin A5
+  #define RIGHT_ENC_PIN_B PC4   //pin A4
 #endif
    
 long readEncoder(int i);
